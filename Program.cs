f9bf96a13ff2f2f26a371025b4e97ec0217bcd0f
MuhDavin213 <davin.new58@gmail.com>
2022-11-28 16:47:04 +0700

    add menu

--- Program.cs
+++ Program.cs
@@ -44,11 +44,40 @@ namespace StackUsingSingleLinkList
             Console.WriteLine("\nThe poped elements is:" + top.info);
             top = top.next;// make top poin to the next node is squence
         }
+        public void display()
+        {
+            node tmp;
+            if (empty())
+                Console.WriteLine("\nStack Empty");
+            else
+            {
+                for (tmp = top; tmp != null; tmp = tmp.next)
+                {
+                    Console.WriteLine(tmp.info);
+                }
+            }
+        }
     }
     internal class Program
     {
         static void Main(string[] args)
         {
+            Stack s = new Stack();
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("\n***Stack Menu***");
+                Console.WriteLine("1. Push");
+                Console.WriteLine("2. Pop");
+                Console.WriteLine("3. Display");
+                Console.WriteLine("4. Exit");
+                Console.Write("\nEnter your choice: ");
+                string input = Console.ReadLine();
+                char ch = Convert.ToChar(input == "" ? "0" : input);
+                switch (ch)
+                {
+                }
+            }
         }
     }
 }
